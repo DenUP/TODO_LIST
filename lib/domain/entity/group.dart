68ea7759b689d45884c834d43356548268fd4598
DenUP <ctrike163@gmail.com>
2024-05-05 19:16:22 +0300

    modif: entity_Group

--- lib/domain/entity/group.dart
+++ lib/domain/entity/group.dart
@@ -4,17 +4,11 @@ import 'package:todo_list/domain/entity/task.dart';
 part 'group.g.dart';
 
 @HiveType(typeId: 1)
+// used id 1
 class Group extends HiveObject {
   @HiveField(0)
   String name;
-  @HiveField(1)
-  HiveList<Task>? tasks;
+
 
   Group({required this.name});
-
-  void addTask(Box<Task> box, Task task) {
-    tasks ??= HiveList(box);
-    tasks?.add(task);
-    save();
-  }
 }
